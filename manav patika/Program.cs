bc5463f7bc8c061e38031460224915669c18ffc6
BARBAROS İLHAN <barbarosthk@gmail.com>
2025-05-08 18:47:23 +0300

    Update Program.cs

--- manav patika/Program.cs
+++ manav patika/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("abc");
+﻿using System;
+namespace MANAVCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rüya Manavına Hoşgeldiniz");
+            Console.WriteLine("Rüya Manavında Fiyatlar Bütçe Dostu!");
+            Console.WriteLine("Elma = 2 TL");
+            Console.WriteLine("Armut = 3 TL");
+            Console.WriteLine("Muz = 3 TL");
+            Console.WriteLine("Çilek = 2TL");
+            Console.WriteLine("Diğer Tüm Meyveler = 4 TL");
+            Console.WriteLine("Hangi Meyvayı Satın Almak İstersiniz?");
+            string meyve = Console.ReadLine().ToLower().Trim();
 
+            if (meyve == "elma")
+            {
+                Console.WriteLine("Seçtiğiniz meyvenin fiyatı 2TL dir.");
+            }
+            else if (meyve == "armut")
+            {
+                Console.WriteLine("seçtiginiz meyvenin fiyatı 3 tl dir.");
+            }
+            else if (meyve == "çilek")
+            {
+                Console.WriteLine("Seçtiginiz meyvenin fiyatı 2 tl dir");
+            }
+            else if (meyve == "muz")
+            {
+                Console.WriteLine("seçtiginiz meyvenin fiyatı 3 TL dir.");
+            }
+
+            else if (meyve == "diger")
+            {
+                Console.WriteLine("diğer tüm meyveler 4 tl dir.");
+            }
+            else
+                Console.WriteLine("Afiyet Olsun");
+        }
+    }
+}
